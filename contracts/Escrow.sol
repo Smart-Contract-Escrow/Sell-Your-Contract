06028389b6882f1d4e60dd1e0906cb8c436939dc
Erik Hanchett <icystorm@gmail.com>
2022-05-21 18:54:33 -0700

    Updated to escrow address

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Escrow is Ownable {
+contract SellYourContract is Ownable {
     address payable public ownerOfContract;
 
     struct SellersInfo {
@@ -19,7 +19,7 @@ contract Escrow is Ownable {
     struct BuyersInfo {
         address contractBeingBought;
         address buyerAddress;
-        uint256 offeringPrice;
+        uint256 sellPrice;
         bool didPayAmount;
     }
 
@@ -29,29 +29,27 @@ contract Escrow is Ownable {
 
     event SellerReady(SellersInfo seller);
     event BuyerReady(BuyersInfo buyer);
+    event TransactionCompleted(BuyersInfo buyer, SellersInfo seller);
 
     constructor() {
         ownerOfContract = payable(msg.sender);
     }
 
-    function setBuyersInfo(
-        address contractBeingBought,
-        address buyerAddress,
-        uint256 offeringPrice
-    ) external payable {
+    function setBuyersInfo(address contractBeingBought) external payable {
+        uint256 amount = msg.value; //amount sent
+        address buyerAddress = msg.sender; //buyer address
+
+        require(amount != 0, "Buyer did not send any amount");
         BuyersInfo memory myBuyersInfo = BuyersInfo(
             contractBeingBought,
             buyerAddress,
-            offeringPrice,
+            amount,
             false
         );
+
         buyers[msg.sender] = myBuyersInfo;
 
-        if (
-            sellers[myBuyersInfo.contractBeingBought].sellerAddress !=
-            address(0)
-        ) {
-            // seller information exists
+        if (buyers2sellers[msg.sender] == address(0)) {
             // link buyer2seller
             buyers2sellers[msg.sender] = myBuyersInfo.contractBeingBought;
         }
@@ -68,21 +66,27 @@ contract Escrow is Ownable {
             mySellerLinkedAddress != address(0),
             "Buyer not linked to seller"
         );
-        require(
-            mySellerInfo.sellPrice == msg.value,
-            "Amount sent doesn't match"
-        );
+        if (mySellerInfo.sellPrice != 0) {
+            require(
+                mySellerInfo.sellPrice == msg.value,
+                "Amount sent doesn't match"
+            );
+        }
 
         myBuyersInfo.didPayAmount = true;
         buyers[msg.sender] = myBuyersInfo;
+
+        emit BuyerReady(myBuyersInfo);
+
+        goodToGo(mySellerInfo, myBuyersInfo);
     }
 
     function setSellersInfo(
         address contractBeingSold,
-        address sellerAddress,
         uint256 sellPrice,
         address buyerAddress
     ) external {
+        address sellerAddress = msg.sender;
         SellersInfo memory mySellerInfo = SellersInfo(
             contractBeingSold,
             sellerAddress,
@@ -92,7 +96,7 @@ contract Escrow is Ownable {
         ); // Buyers Information
         sellers[msg.sender] = mySellerInfo;
 
-        if (buyers[mySellerInfo.buyersAddress].buyerAddress != address(0)) {
+        if (buyers2sellers[mySellerInfo.buyersAddress] != address(0)) {
             // does buyer information already exist?
             // if so update buyers2sellers mapping
             buyers2sellers[mySellerInfo.buyersAddress] = msg.sender;
@@ -105,19 +109,44 @@ contract Escrow is Ownable {
         // SellersInfo memory mySellerInfo = sellers[msg.sender]; // Sellers Information
         address sellersLinkAddress = buyers2sellers[mySellerInfo.buyersAddress]; // Buyers Linked Address
         BuyersInfo memory myBuyerInfo = buyers[mySellerInfo.buyersAddress];
-        require(mySellerInfo.sellerAddress != address(0), "Seller not found");
+
         require(sellersLinkAddress != address(0), "Seller not linked to buyer");
         require(
-            myBuyerInfo.contractBeingBought == msg.sender,
+            myBuyerInfo.contractBeingBought == mySellerInfo.contractBeingSold,
             "Contract address does not match what was sent"
         );
         // call other contract and change manager ERC20
 
         mySellerInfo.sentContract = true;
         sellers[msg.sender] = mySellerInfo;
+        emit SellerReady(mySellerInfo);
+        goodToGo(mySellerInfo, myBuyerInfo);
     }
 
-    function goodToGo() public {}
+    function goodToGo(SellersInfo memory seller, BuyersInfo memory buyer)
+        internal
+    {
+        if (seller.sentContract && buyer.didPayAmount) {
+            require(
+                seller.sellPrice == buyer.sellPrice,
+                "Sell price doesn't match buy price"
+            );
+            require(
+                seller.contractBeingSold == buyer.contractBeingBought,
+                "Contract sold and bought not the same"
+            );
+            // good to go send values
+            // transfer
+            payable(seller.sellerAddress).transfer(buyer.sellPrice);
+            emit TransactionCompleted(buyer, seller);
+        } else {
+            return;
+        }
+    }
+
+    function getBalance() public view onlyOwner returns (uint256) {
+        return address(this).balance;
+    }
 
     function withdraw() public payable onlyOwner {
         uint256 balance = address(this).balance;
