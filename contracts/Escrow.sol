3bc2dd6494b71ac7b846b60404870473d1312a9f
Erik Hanchett <icystorm@gmail.com>
2022-05-21 20:15:52 -0700

    updated contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+/SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -25,21 +25,23 @@ contract SellYourContract is Ownable {
 
     mapping(address => BuyersInfo) public buyers;
     mapping(address => SellersInfo) public sellers;
-    mapping(address => address) public buyers2sellers;
 
     event SellerReady(SellersInfo seller);
-    event BuyerReady(BuyersInfo buyer);
     event TransactionCompleted(BuyersInfo buyer, SellersInfo seller);
 
     constructor() {
         ownerOfContract = payable(msg.sender);
     }
 
-    function setBuyersInfo(address contractBeingBought) external payable {
+    function setBuyersInfo(
+        address contractBeingBought
+    ) external payable {
         uint256 amount = msg.value; //amount sent
         address buyerAddress = msg.sender; //buyer address
 
         require(amount != 0, "Buyer did not send any amount");
+        require(contractBeingBought != address(0), "Contract Not Found");
+
         BuyersInfo memory myBuyersInfo = BuyersInfo(
             contractBeingBought,
             buyerAddress,
@@ -47,46 +49,44 @@ contract SellYourContract is Ownable {
             false
         );
 
-        buyers[msg.sender] = myBuyersInfo;
+        // verify sellers information matches buyers information, seller must go first
+        require(sellers[contractBeingBought].buyersAddress == msg.sender, "Seller address did not match buyer");
 
-        if (buyers2sellers[msg.sender] == address(0)) {
-            // link buyer2seller
-            buyers2sellers[msg.sender] = myBuyersInfo.contractBeingBought;
-        }
+        buyers[contractBeingBought] = myBuyersInfo;
+
+    
         buyerSendPay(myBuyersInfo);
     }
 
     function buyerSendPay(BuyersInfo memory myBuyersInfo) public payable {
-        // BuyersInfo memory myBuyerInfo = buyers[msg.sender]; // Buyers Information
-        address mySellerLinkedAddress = buyers2sellers[msg.sender]; // Sellers Linked Address
-        SellersInfo memory mySellerInfo = sellers[mySellerLinkedAddress]; // Sellers Info
+        SellersInfo memory mySellerInfo = sellers[myBuyersInfo.contractBeingBought]; // Sellers Info
 
-        require(myBuyersInfo.buyerAddress != address(0), "Buyer Not Found");
-        require(
-            mySellerLinkedAddress != address(0),
-            "Buyer not linked to seller"
-        );
-        if (mySellerInfo.sellPrice != 0) {
+        if(mySellerInfo.sellPrice != 0){
             require(
-                mySellerInfo.sellPrice == msg.value,
-                "Amount sent doesn't match"
+              mySellerInfo.sellPrice == msg.value,
+              "Amount sent doesn't match"
             );
         }
+        
 
         myBuyersInfo.didPayAmount = true;
-        buyers[msg.sender] = myBuyersInfo;
+        buyers[myBuyersInfo.contractBeingBought] = myBuyersInfo;
 
-        emit BuyerReady(myBuyersInfo);
 
         goodToGo(mySellerInfo, myBuyersInfo);
+
     }
 
     function setSellersInfo(
-        address contractBeingSold,
+        address contractBeingSold,  
         uint256 sellPrice,
         address buyerAddress
     ) external {
         address sellerAddress = msg.sender;
+
+        require(contractBeingSold != address(0), "Contract Not Found");
+       
+
         SellersInfo memory mySellerInfo = SellersInfo(
             contractBeingSold,
             sellerAddress,
@@ -94,55 +94,48 @@ contract SellYourContract is Ownable {
             buyerAddress,
             false
         ); // Buyers Information
-        sellers[msg.sender] = mySellerInfo;
 
-        if (buyers2sellers[mySellerInfo.buyersAddress] == address(0)) {
-            // does buyer information not already exist?
-            // if so update buyers2sellers mapping
-            buyers2sellers[mySellerInfo.buyersAddress] = msg.sender;
-        }
+        sellers[contractBeingSold] = mySellerInfo;
 
+        
         sellerSendContract(mySellerInfo);
     }
 
     function sellerSendContract(SellersInfo memory mySellerInfo) internal {
-        // SellersInfo memory mySellerInfo = sellers[msg.sender]; // Sellers Information
-        address sellersLinkAddress = buyers2sellers[mySellerInfo.buyersAddress]; // Buyers Linked Address
-        BuyersInfo memory myBuyerInfo = buyers[mySellerInfo.buyersAddress];
-
-        require(sellersLinkAddress != address(0), "Seller not linked to buyer");
+        BuyersInfo memory myBuyerInfo = buyers[mySellerInfo.contractBeingSold];
 
         // call other contract and change manager ERC20
 
         mySellerInfo.sentContract = true;
-        sellers[msg.sender] = mySellerInfo;
+        sellers[mySellerInfo.contractBeingSold] = mySellerInfo;
         emit SellerReady(mySellerInfo);
         goodToGo(mySellerInfo, myBuyerInfo);
     }
 
-    function goodToGo(SellersInfo memory seller, BuyersInfo memory buyer)
-        internal
-    {
-        if (seller.sentContract && buyer.didPayAmount) {
-            require(
-                seller.sellPrice == buyer.sellPrice,
-                "Sell price doesn't match buy price"
-            );
-            require(
-                seller.contractBeingSold == buyer.contractBeingBought,
-                "Contract sold and bought not the same"
-            );
+
+    function goodToGo(SellersInfo memory seller, BuyersInfo memory buyer) internal {
+        if(seller.sentContract && buyer.didPayAmount){
+            require(seller.sellPrice == buyer.sellPrice, "Sell price doesn't match buy price");
+            require(seller.contractBeingSold == buyer.contractBeingBought, "Contract sold and bought not the same");
             // good to go send values
             // transfer
             payable(seller.sellerAddress).transfer(buyer.sellPrice);
             emit TransactionCompleted(buyer, seller);
+            delete sellers[seller.contractBeingSold];
+            delete buyers[buyer.contractBeingBought];
         } else {
             return;
         }
     }
 
-    function getBalance() public view onlyOwner returns (uint256) {
+    function getBalance() public onlyOwner view returns(uint256) {
         return address(this).balance;
+    }
+
+    function getSellerAmount(address myContract) public view returns(uint256) {
+        SellersInfo memory seller =  sellers[myContract];
+        require(seller.buyersAddress == msg.sender, "Only buyer can see amount");
+        return seller.sellPrice;
     }
 
     function withdraw() public payable onlyOwner {
